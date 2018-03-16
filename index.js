ae5f385da580050ba869beb8e263fef5a8e796a2
JackelineGCP <gessenia_17@hotmail.com>
2018-03-16 11:00:25 -0500

    Show hello world in development&production environment

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'; // Permite crear componentes
+import ReactDom from 'react-dom'; // Permite renderizar elementos o componentes en el navegador
+
+const app = document.getElementById('app');
+const holaMundo = <h1> Hola mundo </h1>;
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+ReactDom.render(holaMundo, app);
