ab9a0ea43208b60b704c29162265361a2558dc0b
JackelineGCP <gessenia_17@hotmail.com>
2018-03-19 12:46:28 -0500

    linking bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';  // Permite crear componentes
 import ReactDom from 'react-dom'; // Permite renderizar elementos o componentes en el navegador
-
+import App from './src/app'
+/* 
 const app = document.getElementById('app');
 const holaMundo = <h1>Hola mundo</h1>;
-
+ */
 // ReactDOM.render(que voy a renderizar, donde lo haré);
-ReactDom.render(holaMundo, app);
+ReactDom.render (<App/>,document.getElementById('app'));
