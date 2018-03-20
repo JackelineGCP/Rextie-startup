13b8f2b1d1184079651b5a347b9ab81c0652e41b
JackelineGCP <gessenia_17@hotmail.com>
2018-03-20 13:36:00 -0500

    Cleaning code

--- src/app.js
+++ src/app.js
@@ -1,25 +1,10 @@
-/* import React from 'react';
+import React from 'react';
 
 export default () => (
   <div className="container">
     Hola, soy react
-    <button className="btn btn-primary">aceptar</button>
+    <button type="button" className="btn btn-raised btn-warning">Warning</button>
   </div>
 
-); */
+); 
 
-import React from 'react';
-import Header from './header'
-/* import Footer from './Footer'
-import Main from './Main'
-import Notification from './Notification'; */
-
-const App = () => (
-
-  <div className="app">
-    <Header />
-  </div>
-
-)
-
-export default App;
