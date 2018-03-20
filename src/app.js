eb91bb2247f88bc3d45929cf67adf57f2fe8fce6
JackelineGCP <gessenia_17@hotmail.com>
2018-03-20 13:29:25 -0500

    Add bootstrap material disign

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 
 export default () => (
   <div className="container">
@@ -6,5 +6,20 @@ export default () => (
     <button className="btn btn-primary">aceptar</button>
   </div>
 
-);
+); */
 
+import React from 'react';
+import Header from './header'
+/* import Footer from './Footer'
+import Main from './Main'
+import Notification from './Notification'; */
+
+const App = () => (
+
+  <div className="app">
+    <Header />
+  </div>
+
+)
+
+export default App;
