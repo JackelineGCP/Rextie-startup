f988710e5ed061ee7aff074b05dd14f47caf1a6c
Danielapariona <daniela.parionac@gmail.com>
2018-03-21 16:56:52 -0500

    Refactor code in view operation

--- src/sidebar/components/sidebar.js
+++ src/sidebar/components/sidebar.js
@@ -2,59 +2,42 @@ import React from 'react'
 import './sidebar.css'
 
 const SideBar = () => {
-  return(
-  <div className="msb" id="msb">
-    <nav className="navbar navbar-default" role="navigation">
-      <div className="navbar-header">
-        <div className="brand-wrapper">
-          <div className="brand-name-wrapper">
-            <a className="navbar-brand" href="#">
-              Rextie
+  return (
+    <div className="msb" id="msb">
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="navbar-header">
+          <div className="brand-wrapper">
+            <div className="brand-name-wrapper">
+              <a className="navbar-brand" href="#">
+                Rextie
               </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="side-menu-container">
-        <ul className="nav navbar-nav">
-          <li><a href="#"><i className="fa fa-dashboard"></i> Dashboard</a></li>
-          <li className="active"><a href="#"><i className="fa fa-puzzle-piece"></i> Components</a></li>
-          <li><a href="#"><i className="fa fa-heart"></i> Extras</a></li>
-          <li className="panel panel-default" id="dropdown">
-            <a data-toggle="collapse" href="#dropdown-lvl1">
-              <i className="fa fa-diamond"></i> Apps
+        <div className="side-menu-container">
+          <ul className="nav navbar-nav">
+            <li className='button-primary'><a href="#"><i className="glyphicon glyphicon-random
+"></i> Nueva operación</a></li>
+            <li className="active"><a href="#"><i className="glyphicon glyphicon-signal"></i> Principal</a></li>
+            <li className="panel panel-default" id="dropdown">
+              <a data-toggle="collapse" href="#dropdown-lvl1">
+                <i className="fa fa-id-card"></i>Cuentas bancarias
                 <span className="caret"></span>
-            </a>
-            <div id="dropdown-lvl1" className="panel-collapse collapse">
-              <div className="panel-body">
-                <ul className="nav navbar-nav">
-                  <li><a href="#">Mail</a></li>
-                  <li><a href="#">Calendar</a></li>
-                  <li><a href="#">Ecommerce</a></li>
-                  <li><a href="#">User</a></li>
-                  <li><a href="#">Social</a></li>
-                  <li className="panel panel-default" id="dropdown">
-                    <a data-toggle="collapse" href="#dropdown-lvl2">
-                      <i className="glyphicon glyphicon-off"></i> Sub Level <span className="caret"></span>
-                    </a>
-                    <div id="dropdown-lvl2" className="panel-collapse collapse">
-                      <div className="panel-body">
-                        <ul className="nav navbar-nav">
-                          <li><a href="#">Link</a></li>
-                          <li><a href="#">Link</a></li>
-                          <li><a href="#">Link</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
+              </a>
+              <div id="dropdown-lvl1" className="panel-collapse collapse">
+                <div className="panel-body">
+                  <ul className="nav navbar-nav">
+                    <li><a href="#">Cuentas bancarias personales</a></li>
+                    <li><a href="#">Cuentas bancarias de destino</a></li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          </li>
-          <li><a href="#"><span className="glyphicon glyphicon-signal"></span> Link</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
+            </li>
+            <li><a href="#"><span className="fa fa-history"></span>Historial de operaciones</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   )
 }
 
