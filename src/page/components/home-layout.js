39fa64bb81e47b33da29f93c810b93b6dd913f1f
JackelineGCP <gessenia_17@hotmail.com>
2018-03-22 08:18:52 -0500

    Add branch cuenta-ui

--- src/page/components/home-layout.js
+++ src/page/components/home-layout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../../header/components/header';
 import Banners from '../../banners/components/banners';
 import Footer from '../../footer/components/footer'
+import DatosdeCuenta from '../../datosdecuenta/components/datosdecuenta';
+
 
 const HomeLayout = () => {
   return (
@@ -9,6 +11,7 @@ const HomeLayout = () => {
       <Header />
       <Banners />
       <Footer />
+      <DatosdeCuenta />
     </div>
   )
 }
